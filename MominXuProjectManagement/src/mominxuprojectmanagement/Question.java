a484409eebfd5693bd5fa61db8cae8d2314a5b85
mominkaif <kaifmomin30@gmail.com>
2022-05-11 13:34:26 -0400

    new question class

---
+++ MominXuProjectManagement/src/mominxuprojectmanagement/Question.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mominxuprojectmanagement;
+
+/**
+ *
+ * @author kaifm
+ */
+public class Question {
+
+    private String q;
+    private String optA, optB, optC, optD, ans;
+
+    public Question() {
+        q = "";
+        optA = "";
+        optB = "";
+        optC = "";
+        optD = "";
+        ans = "";
+    }
+
+    public Question(String q, String optA, String optB, String optC, String optD, String ans) {
+        q = "";
+        optA = "";
+        optB = "";
+        optC = "";
+        optD = "";
+        ans = "";
+    }
+}
